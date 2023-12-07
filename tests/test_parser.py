bcd59e23e738ad3843d447172c2c09b246fa37a3
ant100pshnik <mypochtav@gmail.com>
2023-12-07 22:38:06 +0200

    add more tests to cover all cases given in Lab 1

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -53,9 +53,77 @@ class TestParser(unittest.TestCase):
 
         lib = Lib(x=2.5, y=2 * math.pi)
         expression = "1.2 + sin(y * x)"
-        required = round(1.2 + math.sin(2 * math.pi * 2.5), 2)
-        given = round(Parser.parse(expression).eval(ReflectionContext(lib)), 2)
-        self.assertEqual(given, required)
+        required = 1.2 + math.sin(2 * math.pi * 2.5)
+        given = Parser.parse(expression).eval(ReflectionContext(lib))
+        self.assertAlmostEqual(given, required)
 
         if __name__ == '__main__':
             unittest.main()
+
+    def test_errors_on_start(self):
+        # parentheses
+        expression = ") 5 + x"
+        with self.assertRaises(SyntaxError):
+            Parser.parse(expression)
+        # / or *
+        expression = "/ 5 + x"
+        with self.assertRaises(SyntaxError):
+            Parser.parse(expression)
+
+    def test_incorrect_names(self):
+        class Lib:
+            def __init__(self, x: float, y: float):
+                self.x = x
+                self.y = y
+
+            @staticmethod
+            def sin(x: float):
+                return math.sin(x)
+
+        lib = Lib(x=2.5, y=2 * math.pi)
+        # check error if we have incorrect variables names
+        expression = "1.2 + sin(a * b)"
+        with self.assertRaises(AttributeError):
+            Parser.parse(expression).eval(ReflectionContext(lib))
+        # check error if we have incorrect function  name
+        expression = "1.2 + foo(a * b)"
+        with self.assertRaises(AttributeError):
+            Parser.parse(expression).eval(ReflectionContext(lib))
+
+    def test_errors_on_end(self):
+        # closed parenthesis on end
+        expression = "5 + x ("
+        with self.assertRaises(SyntaxError):
+            Parser.parse(expression)
+        # end with operator
+        expression = "5 + x *"
+        with self.assertRaises(SyntaxError):
+            Parser.parse(expression)
+
+    def test_errors_on_middle(self):
+        # double operations
+        expression = "5 + + x"
+        with self.assertRaises(SyntaxError):
+            Parser.parse(expression)
+        # no operation before brackets
+        expression = "5 ( x + 1 )"
+        with self.assertRaises(SyntaxError):
+            Parser.parse(expression)
+        # forbidden operation after opened bracket
+        expression = "5 ( / x + 1 )"
+        with self.assertRaises(SyntaxError):
+            Parser.parse(expression)
+
+    def test_parentheses(self):
+        # redundant closed parenthesis
+        expression = "5 + (sin(0) + x))"
+        with self.assertRaises(SyntaxError):
+            Parser.parse(expression)
+        # empty parenthesis
+        expression = "5 + ()"
+        with self.assertRaises(SyntaxError):
+            Parser.parse(expression)
+
+
+if __name__ == "__main__":
+    unittest.main()
