08b1754248911276b743ae731d27c1c66e5396b0
ant100pshnik <mypochtav@gmail.com>
2023-12-07 20:31:02 +0200

    add test of evaluator

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,8 +1,11 @@
+import math
 from io import StringIO
 import unittest
 
 from enums import Token
+from parser import Parser
 from tokenizer import Tokenizer
+from context import ReflectionContext
 
 
 class TestParser(unittest.TestCase):
@@ -38,6 +41,21 @@ class TestParser(unittest.TestCase):
         self.assertEqual(t.token, Token.CLOSE_PARENS)
         t.next_token()
 
+    def test_evaluator(self):
+        class Lib:
+            def __init__(self, x: float, y: float):
+                self.x = x
+                self.y = y
 
-if __name__ == '__main__':
-    unittest.main()
+            @staticmethod
+            def sin(x: float):
+                return math.sin(x)
+
+        lib = Lib(x=2.5, y=2 * math.pi)
+        expression = "1.2 + sin(y * x)"
+        required = round(1.2 + math.sin(2 * math.pi * 2.5), 2)
+        given = round(Parser.parse(expression).eval(ReflectionContext(lib)), 2)
+        self.assertEqual(given, required)
+
+        if __name__ == '__main__':
+            unittest.main()
