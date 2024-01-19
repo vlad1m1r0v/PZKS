7cf419f1f46d73e95df50907db0cc96c970ba731
ant100phnik <mypochtav@gmail.com>
2024-01-19 22:15:57 +0200

    fix bug with undetecting of closing parentheses in the end

--- expression_engine/validator.py
+++ expression_engine/validator.py
@@ -184,7 +184,7 @@ class State:
                               f"of type {token_name(cur)} and value {cur.value}")
                 return
 
-            if self._open_paren:
+            if self._open_paren and cur.type != Token.CLOSE_PARENS:
                 self.complain(f"Error at {cur.matched_at}: "
                               f"missing close parens for parens from {self._open_paren.matched_at}")
                 return
